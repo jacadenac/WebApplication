c21b165d609be5376e7a55643cb3b643d2218b9d
William Andrés Montañez <willi.1256@gmail.com>
2013-06-13 22:14:53 -0500

    continuación controladorcitas
    adecuanción de métodos

--- src/java/beans/ControladorCita.java
+++ src/java/beans/ControladorCita.java
@@ -12,34 +12,62 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
 import javax.inject.Named;
 
 /**
  *
  * @author Alejandro
  */
-@Named(value = "hospitalBean")
+@Named(value = "ControladorCita")
+@ManagedBean
 @RequestScoped
-public class hospitalBean {
+public class ControladorCita {
     @EJB
     Conector conector;
 
-    private List<Hospital> hospitales;
+    private List<Hospital> hospitales = new ArrayList<Hospital>();
+    private List<Doctor> doctores;
+    private List<Cita> citas;
     private Hospital selectedHospital;
     private Cita selectedCita;
-    private String selectedTipoCita;
+    private String selectedEspecialidad;
     private Doctor selectedDoctor;
 
     //método editado
-    public hospitalBean() {
+    public ControladorCita() {
+        
         this.hospitales = new ArrayList<Hospital>();
-        selectedHospital = new Hospital(3, "HOSPITAL TUNAL");
-        selectedCita = new Cita();
-        selectedDoctor = new Doctor();
+        this.selectedHospital = new Hospital(3, "HOSPITAL TUNAL");
+        this.doctores = new ArrayList<Doctor>();
+        this.selectedDoctor = new Doctor();
+        this.citas = new ArrayList<Cita>();
+        this.selectedCita = new Cita();
     }
 
     public List<Hospital> getHospitales() {
-        return conector.getHospitales();
+        hospitales= conector.getHospitales();
+        return hospitales;
+    }
+
+    public void setHospitales(List<Hospital> hospitales) {
+        this.hospitales = hospitales;
+    }
+
+    public List<Doctor> getDoctores() {
+        return doctores;
+    }
+
+    public void setDoctores(List<Doctor> doctores) {
+        this.doctores = doctores;
+    }
+
+    public List<Cita> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(List<Cita> citas) {
+        this.citas = citas;
     }
 
     public Hospital getSelectedHospital() {
@@ -58,6 +86,14 @@ public class hospitalBean {
         this.selectedCita = selectedCita;
     }
 
+    public String getSelectedEspecialidad() {
+        return selectedEspecialidad;
+    }
+
+    public void setSelectedEspecialidad(String selectedTipoCita) {
+        this.selectedEspecialidad = selectedTipoCita;
+    }
+
     public Doctor getSelectedDoctor() {
         return selectedDoctor;
     }
@@ -66,19 +102,12 @@ public class hospitalBean {
         this.selectedDoctor = selectedDoctor;
     }
 
-    public String getSelectedTipoCita() {
-        return selectedTipoCita;
+    public void actualizarDoctores() {
+        if(selectedHospital!=null) {
+            doctores = conector.getDoctores(selectedHospital.getId(),selectedEspecialidad);
+        }
     }
 
-    public void setSelectedTipoCita(String selectedTipoCita) {
-        this.selectedTipoCita = selectedTipoCita;
-    }   
-    
-    //métodos para personalizar la búsqueda
-    public List<Doctor> getDoctores() {
-        return conector.getDoctores(selectedHospital.getId(),selectedTipoCita);
-    }
-    
     public void btnCreateHospital(ActionEvent actionEvent) {
         //implementar método que se comunica con el otro compomente
     }
